33d8f0c5a6478ad457f52e619b553be299f0f143
dev <devarshi.ap@gmail.com>
2022-01-04 21:52:31 -0500

    create movie-schema validation

---
+++ models/movie.js
@@ -0,0 +1,60 @@
+/**
+ * Creating Schema Validations for each movie document in the 'movies' collection
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var movieSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+
+  // _id not necessarily required since MongoDB Atlas autogenerates ObjectId() for document if not specified
+  _id: Number,
+
+  // 5 phases announced in MCU
+  phase: {
+    type: Number,
+    min: 1,
+    max: 5,
+    required: true,
+  },
+
+  // IMDb scale 0-10, with -1 indicating not yet rated
+  IMDb_rating: {
+    type: Number,
+    min: -1,
+    max: 10,
+    required: true,
+  },
+
+  release_date: String,
+  
+  release_year: Number,
+
+  running_time: Number,
+  
+  // ratings per Motion Picture Assc. (MPA Canada)
+  rating_name: {
+    type: String,
+    enum: ['G', 'PG', 'PG-13', '14A', '18A', 'R', 'tbd'],
+  },
+  
+  watched: {
+    type: Boolean,
+    required: true,
+  },
+  
+  budget: Number,
+  
+  gross: Number,
+  
+  misc_tags: [{ type: String }],
+});
+
+// compile model from above schema
+var movieModel = mongoose.model('movie', movieSchema)
+
+

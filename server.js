f6e670f96bb2eb9aa7956c6835e79042a4d08dc7
dev <devarshi.ap@gmail.com>
2022-01-08 21:31:36 -0500

    hiding sensitive keys with .env

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// load environment variables from .env file
+require('dotenv').config();
+
 // import modules
 const express = require('express');
 const mongoose = require('mongoose');
@@ -5,9 +8,7 @@ const morgan = require('morgan');
 const PORT = 3000;
 
 // connect mongoose to mongodb cluster
-const db_uri =
-  'mongodb+srv://m001-student:m001-mongodb-basics@sandbox.2f5f6.mongodb.net/sandbox?retryWrites=true&w=majority';
-mongoose.connect(db_uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // set up express app
 const app = express();
